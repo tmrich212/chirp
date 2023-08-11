a0822c784f40f20da609007ae784404ee2864b46
tmrich212 <57809945+tmrich212@users.noreply.github.com>
2023-08-11 14:26:04 -0400

    Posting loading states + toast

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 
 dayjs.extend(relativeTime)
@@ -23,6 +24,14 @@ const CreatePostWizard = () => {
     onSuccess: () => {
       setInput("");
       void ctx.posts.getAll.invalidate();
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0])
+      } else {
+        toast.error("Failed to post! Try again later.")
+      }
     }
   });
   
@@ -47,10 +56,26 @@ const CreatePostWizard = () => {
     type="text"
     value={input}
     onChange={(e) => setInput(e.target.value)}
+
+    onKeyDown={(e) => {
+      if(e.key === "Enter"){
+        e.preventDefault();
+        if(input !== ""){
+          mutate({ content: input })
+        }
+      }
+    }}
     disabled={isPosting}
     />
 
-    <button onClick={() => mutate({content: input})}>Post</button>
+    {input !== "" &&  !isPosting && (
+    <button onClick={() => mutate({ content: input })}>Post</button>
+    )}
+
+    {isPosting && (
+    <div className="flex items-center justify-center">
+      <LoadingSpinner size={20}/>
+    </div>)}
   </div>)
 }
 
