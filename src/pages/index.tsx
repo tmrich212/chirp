0aa2fe94572ff92c57e06ac827e23003868a078b
tmrich212 <57809945+tmrich212@users.noreply.github.com>
2023-08-15 14:46:04 -0400

    Console log test for user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,7 +39,7 @@ const CreatePostWizard = () => {
   
   const [input, setInput] = useState("");
 
-  // console.log(user);
+  console.log(user);
 
   if(!user) return null;
 
@@ -125,3 +125,5 @@ export default function Home() {
     </>
   );
 }
+
+//test comment to push to vercel
